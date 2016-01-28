d415d670bf5b9817754f6c24a576d49bd9792e45
MANIKANDAN KAR <manikandan_kar@mm-manikandan-kar.local>
2016-01-28 12:49:24 +0530

    Sample Class 1

---
+++ Pod/Classes/SampleClass/SampleClass.h
@@ -0,0 +1,13 @@
+//
+//  SampleClass.h
+//  Pods
+//
+//  Created by MANIKANDAN_KAR on 28/01/16.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SampleClass : NSObject
+
+@end
---
+++ Pod/Classes/SampleClass/SampleClass.m
@@ -0,0 +1,13 @@
+//
+//  SampleClass.m
+//  Pods
+//
+//  Created by MANIKANDAN_KAR on 28/01/16.
+//
+//
+
+#import "SampleClass.h"
+
+@implementation SampleClass
+
+@end
